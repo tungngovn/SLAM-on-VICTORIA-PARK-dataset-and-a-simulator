1a140d7516abeeb5eb172c0749f69795e5063c40
tunghustvn <tungsd2@gmail.com>
2021-02-08 20:30:07 +0700

    Add g2o

--- algorithm.md
+++ algorithm.md
@@ -2,22 +2,47 @@
 
 Data = load_vp_si();
 
+```
+Data.Laser.ranges = double(LASER)/100; % [m]
+Data.Laser.time = double(TLsr)/1000;   % [s]
+Data.Control.ve = speed;               % [m/s]
+Data.Control.alpha = steering;         % [rad]
+Data.Control.time = time/1000;         % [s]
+Data.Gps.x = Lo_m;                     % [m]
+Data.Gps.y = La_m;                     % [m]
+Data.Gps.time = timeGps/1000;          % [s]
+```
+
 # Initialization
-set_threshold;
+
 ci =1;
+
 t = min(Data.Laser.time(1), Data.Control.time(1));
+
 map = init_map();
+
 M = init_local_map();
 
+set_threshold;
+
 # For to scan all the data
+
 for k=1:length(Data.Laser.time)
+
         while (Data.Control.time(ci) < Data.Laser.time(k))
+        
                 X_p = motion_model(u(ci), t); 
+                
                 ci++;
+                
                 if (new_XY_vertex): {add_XY_vertex; add_XY_edge}
+                
         Z = detect_tree(laser_range);
+        
         T = NICP(M, Z);
+        
         X, M = T.*(X_p, M_p);
+        
         if (new_vertex(X, M)): {add_SE2_vertex; add_SE2_edge}
 
 
@@ -26,4 +51,6 @@ for k=1:length(Data.Laser.time)
 
 
 # Add vertex
+
 write_vertex_to_
+
---
+++ motion_model.m
@@ -0,0 +1,21 @@
+function X_p = motion_model(u, dt)
+% Motion model for Victoria Park dataset
+global Param;
+%global State;
+X_p = zeros(3,1);
+%F = [eye(3), zeros(3,size(State.Ekf.mu,1)-3)];
+ve = u(1,1);
+alpha = u(2,1);
+vc = ve/(1-tan(alpha)*(Param.H/Param.L)); %% CALCULATE VC
+%phi = State.Ekf.mu(3,1);
+phi = 36*pi/180;
+X_p(1,1) = dt*(vc*cos(phi)-(vc/Param.L)*tan(alpha)*(Param.a*sin(phi)+Param.b*cos(phi)))+ Param.Qu(1,1); %%CHANGE IN X
+X_p(2,1) = dt*(vc*sin(phi)+(vc/Param.L)*tan(alpha)*(Param.a*cos(phi)-Param.b*sin(phi)))+ Param.Qu(1,1); %% CHANGE IN Y
+X_p(3,1) = dt*(vc/Param.L)*tan(alpha) + Param.Qu(2,2); %CHANGE IN THETA
+%State.Ekf.predMu = State.Ekf.mu + F'*motion_model; %%%UPDATE MU ACCORDING TO THE GIVEN MOTION MODEL
+%State.Ekf.predMu(3,1) = minimizedAngle(State.Ekf.predMu(3,1));
+%G = eye(size(F,2)) + F'* [0,0, -dt*(vc*sin(phi)+(vc/Param.L)*tan(alpha)*(Param.a*cos(phi)-Param.b*sin(phi)));... %deriv. of process model
+%                          0,0, dt*(vc*cos(phi)-(vc/Param.L)*tan(alpha)*(Param.a*sin(phi)+Param.b*cos(phi)));...
+%                          0,0,0;]*F;             
+%State.Ekf.predSigma = G * State.Ekf.Sigma * G' + F' * Param.Qf * F ;  %%UPDATE SIGMA ACCORDING TO THE MOTION MODEL
+end
